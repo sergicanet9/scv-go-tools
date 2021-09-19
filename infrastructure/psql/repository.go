1b6594a52a53726d4aea8ecd5237ec24ab1fb84d
Sergi Canet <s.canet9@outlook.com>
2021-09-19 23:16:01 +0200

    improvements in psql repository

--- infrastructure/psql/repository.go
+++ infrastructure/psql/repository.go
@@ -7,10 +7,10 @@ import (
 
 // PsqlRepository interface represents a postgres repository
 type PsqlRepository interface {
-	Create(ctx context.Context, entity interface{}) (int, error)
-	Get(ctx context.Context, where string) ([]interface{}, error)
+	Create(ctx context.Context, entity interface{}) (interface{}, error)
+	Get(ctx context.Context, where string) (interface{}, error)
 	GetByID(ctx context.Context, ID int) (interface{}, error)
-	Update(ctx context.Context, ID int, entity interface{}) error
+	Update(ctx context.Context, ID int, entity interface{}) (interface{}, error)
 	Delete(ctx context.Context, ID int) error
 	Transaction(tx *sql.Tx) error
 }
