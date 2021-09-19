f336a79a0d3a32debad375c7f63681cc973829d7
Sergi Canet <s.canet9@outlook.com>
2021-09-19 22:02:08 +0200

    change id type in psql repository interface

--- infrastructure/psql/repository.go
+++ infrastructure/psql/repository.go
@@ -10,7 +10,7 @@ type PsqlRepository interface {
 	Create(ctx context.Context, entity interface{}) (int, error)
 	Get(ctx context.Context, where string) ([]interface{}, error)
 	GetByID(ctx context.Context, ID int) (interface{}, error)
-	Update(ctx context.Context, ID string, entity interface{}) error
-	Delete(ctx context.Context, ID string) error
+	Update(ctx context.Context, ID int, entity interface{}) error
+	Delete(ctx context.Context, ID int) error
 	Transaction(tx *sql.Tx) error
 }
