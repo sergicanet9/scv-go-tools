61464e937fa4fa7ac48f5510ff05bf0151d1649c
Sergi Canet <s.canet9@outlook.com>
2021-12-25 17:22:51 +0100

    fix jwt middleware

--- api/utils/middlewares.go
+++ api/utils/middlewares.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/context"
@@ -11,23 +12,28 @@ import (
 //JWTMiddleware is a middleware function to check the authorization JWT Bearer token header of the request
 func JWTMiddleware(next http.Handler, secret string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bearerToken := r.Header.Get("authorization")
-		if bearerToken != "" {
-			token, err := jwt.Parse(bearerToken, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("there was an error")
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader != "" {
+			bearerToken := strings.Split(authorizationHeader, " ")
+			if len(bearerToken) == 2 {
+				token, err := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("there was an error")
+					}
+					return []byte(secret), nil
+				})
+				if err != nil {
+					ResponseError(w, r, http.StatusUnauthorized, err.Error())
+					return
 				}
-				return []byte(secret), nil
-			})
-			if err != nil {
-				ResponseError(w, r, http.StatusUnauthorized, err.Error())
-				return
-			}
-			if token.Valid {
-				context.Set(r, "decoded", token.Claims)
-				next.ServeHTTP(w, r)
+				if token.Valid {
+					context.Set(r, "decoded", token.Claims)
+					next.ServeHTTP(w, r)
+				} else {
+					ResponseError(w, r, http.StatusUnauthorized, "invalid authorization token")
+				}
 			} else {
-				ResponseError(w, r, http.StatusUnauthorized, "invalid authorization token")
+				ResponseError(w, r, http.StatusUnauthorized, "authorization header not properly formated, should be Bearer + {token}")
 			}
 		} else {
 			ResponseError(w, r, http.StatusUnauthorized, "an authorization header is required")
