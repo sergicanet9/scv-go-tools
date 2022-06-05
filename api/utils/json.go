2d4776e6a3b2978932b1231e7c944b2b803ae92f
Sergi Canet <s.canet9@outlook.com>
2022-06-05 20:01:49 +0200

    improvements in json utils and body logging

--- api/utils/json.go
+++ api/utils/json.go
@@ -2,22 +2,12 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 // ResponseJSON makes the response with payload as json format
-func ResponseJSON(w http.ResponseWriter, r *http.Request, status int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-
-		log.Print(fmt.Sprintf("REQUEST %s:%s, RESPONSE %d: %s", r.Method, r.URL.String(), http.StatusInternalServerError, err.Error()))
-		return
-	}
-
+func ResponseJSON(w http.ResponseWriter, r *http.Request, body []byte, status int, payload interface{}) {
 	for name, values := range r.Header {
 		if name != "Content-Length" {
 			for _, value := range values {
@@ -27,12 +17,19 @@ func ResponseJSON(w http.ResponseWriter, r *http.Request, status int, payload in
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Write([]byte(response))
 
-	log.Print(fmt.Sprintf("REQUEST %s:%s, RESPONSE %d: %s", r.Method, r.URL.String(), status, string(response)))
+	response, err := json.Marshal(payload)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		log.Printf("Response paylod could not be marshalled")
+		log.Printf("REQUEST %s:%s\n BODY: %s\n RESPONSE %d: %s", r.Method, r.URL.String(), string(body), status, err.Error())
+		return
+	}
+	w.Write([]byte(response))
+	log.Printf("REQUEST %s:%s\n BODY: %s\n RESPONSE %d: %s", r.Method, r.URL.String(), string(body), status, string(response))
 }
 
 // ResponseError makes the error response with payload as json format
-func ResponseError(w http.ResponseWriter, r *http.Request, status int, message string) {
-	ResponseJSON(w, r, status, map[string]string{"error": message})
+func ResponseError(w http.ResponseWriter, r *http.Request, body []byte, status int, message string) {
+	ResponseJSON(w, r, body, status, map[string]string{"error": message})
 }
