d875a4ce77bb87ba04da3cbca43dd63aa68c7cc4
Sergi Canet <s.canet9@outlook.com>
2022-07-02 14:02:29 +0200

    fix visibility issue in adapters

--- infrastructure/adapters.go
+++ infrastructure/adapters.go
@@ -13,19 +13,19 @@ import (
 
 // PostgresRepository struct of a mongo repository. Needs a specific implementation for every Repository.
 type PostgresRepository struct {
-	db *sql.DB
+	DB *sql.DB
 }
 
 // MongoRepository struct of a mongo repository. Already implementing the Repository interface.
 type MongoRepository struct {
-	db         *mongo.Database
-	collection *mongo.Collection
-	target     interface{}
+	DB         *mongo.Database
+	Collection *mongo.Collection
+	Target     interface{}
 }
 
 // Create creates an entity in the repository's collection
 func (r *MongoRepository) Create(ctx context.Context, entity interface{}) (string, error) {
-	result, err := r.collection.InsertOne(ctx, entity)
+	result, err := r.Collection.InsertOne(ctx, entity)
 	if err != nil {
 		return "", err
 	}
@@ -43,13 +43,13 @@ func (r *MongoRepository) Get(ctx context.Context, filter map[string]interface{}
 	if take != nil {
 		take64 = int64(*take)
 	}
-	cur, err := r.collection.Find(ctx, filter, &options.FindOptions{Skip: &skip64, Limit: &take64})
+	cur, err := r.Collection.Find(ctx, filter, &options.FindOptions{Skip: &skip64, Limit: &take64})
 	if err != nil {
 		return nil, err
 	}
 
 	for cur.Next(ctx) {
-		entry := reflect.New(reflect.TypeOf(r.target)).Interface()
+		entry := reflect.New(reflect.TypeOf(r.Target)).Interface()
 		if err := cur.Decode(entry); err != nil {
 			return nil, err
 		}
@@ -61,7 +61,7 @@ func (r *MongoRepository) Get(ctx context.Context, filter map[string]interface{}
 
 // GetByID get the document with the specified ID in the repository's collection
 func (r *MongoRepository) GetByID(ctx context.Context, ID string) (interface{}, error) {
-	result := reflect.New(reflect.TypeOf(r.target)).Interface()
+	result := reflect.New(reflect.TypeOf(r.Target)).Interface()
 
 	_id, err := primitive.ObjectIDFromHex(ID)
 	if err != nil {
@@ -69,7 +69,7 @@ func (r *MongoRepository) GetByID(ctx context.Context, ID string) (interface{}, 
 	}
 
 	filter := bson.M{"_id": _id}
-	err = r.collection.FindOne(ctx, filter).Decode(result)
+	err = r.Collection.FindOne(ctx, filter).Decode(result)
 	if err != nil {
 		return nil, err
 	}
@@ -86,7 +86,7 @@ func (r *MongoRepository) Update(ctx context.Context, ID string, entity interfac
 
 	filter := bson.M{"_id": _id}
 	update := bson.M{"$set": entity}
-	result, err := r.collection.UpdateOne(ctx, filter, update)
+	result, err := r.Collection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		return err
 	}
@@ -105,7 +105,7 @@ func (r *MongoRepository) Delete(ctx context.Context, ID string) error {
 	}
 
 	filter := bson.M{"_id": _id}
-	result, err := r.collection.DeleteOne(ctx, filter)
+	result, err := r.Collection.DeleteOne(ctx, filter)
 	if err != nil {
 		return err
 	}
