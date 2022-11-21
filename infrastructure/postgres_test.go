bcfb77c7c07510ad1d7efc4bb993fedd602c9102
Sergi Canet <s.canet9@outlook.com>
2022-11-21 01:46:19 +0100

    modify coverage to test badge

--- infrastructure/postgres_test.go
+++ infrastructure/postgres_test.go
@@ -1,42 +1,35 @@
 package infrastructure
 
-import (
-	"testing"
+// // TestConnectPostgresDB_InvalidConnection checks that ConnectPostgresDB returns an error when an invalid connection string is provided
+// func TestConnectPostgresDB_InvalidConnection(t *testing.T) {
+// 	// Arrange
+// 	expectedError := "missing \"=\" after \"invalid-connection\" in connection info string\""
 
-	"github.com/sergicanet9/scv-go-tools/v3/mocks"
-	"github.com/stretchr/testify/assert"
-)
+// 	// Act
+// 	_, err := ConnectPostgresDB("invalid-connection")
 
-// TestConnectPostgresDB_InvalidConnection checks that ConnectPostgresDB returns an error when an invalid connection string is provided
-func TestConnectPostgresDB_InvalidConnection(t *testing.T) {
-	// Arrange
-	expectedError := "missing \"=\" after \"invalid-connection\" in connection info string\""
+// 	// Assert
+// 	assert.Equal(t, expectedError, err.Error())
+// }
 
-	// Act
-	_, err := ConnectPostgresDB("invalid-connection")
+// // TestPingSql_Ok checks that pingSql does not return an error when a valid db is received
+// func TestPingSql_Ok(t *testing.T) {
+// 	// Arrange
+// 	_, db := mocks.NewSqlDB(t)
+// 	// Act
+// 	err := pingSql(db, nil)
 
-	// Assert
-	assert.Equal(t, expectedError, err.Error())
-}
+// 	// Assert
+// 	assert.Equal(t, nil, err)
+// }
 
-// TestPingSql_Ok checks that pingSql does not return an error when a valid db is received
-func TestPingSql_Ok(t *testing.T) {
-	// Arrange
-	_, db := mocks.NewSqlDB(t)
-	// Act
-	err := pingSql(db, nil)
+// // TestPingSql_Ok checks that pingSql returns an error when a nil db is received
+// func TestPingSql_NilDB(t *testing.T) {
+// 	// Arrange
+// 	expectedError := "an unexpected error happened while opening the connection: %!s(<nil>)"
+// 	// Act
+// 	err := pingSql(nil, nil)
 
-	// Assert
-	assert.Equal(t, nil, err)
-}
-
-// TestPingSql_Ok checks that pingSql returns an error when a nil db is received
-func TestPingSql_NilDB(t *testing.T) {
-	// Arrange
-	expectedError := "an unexpected error happened while opening the connection: %!s(<nil>)"
-	// Act
-	err := pingSql(nil, nil)
-
-	// Assert
-	assert.Equal(t, expectedError, err.Error())
-}
+// 	// Assert
+// 	assert.Equal(t, expectedError, err.Error())
+// }
