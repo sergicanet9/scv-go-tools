2b5bffb49905055b267304bbc4ce43593b32ee68
Sergi Canet <sergi.canet@derivco.se>
2025-09-02 11:20:46 +0200

    add missing comments

--- mocks/db.go
+++ mocks/db.go
@@ -8,10 +8,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
 
+// NewMongoDB returns a new MongoDB mock
 func NewMongoDB(t *testing.T) *mtest.T {
 	return mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 }
 
+// NewSqlDB returns a new SQL DB mock
 func NewSqlDB(t *testing.T) (sqlMock sqlmock.Sqlmock, db *sql.DB) {
 	db, sqlMock, _ = sqlmock.New()
 	return
